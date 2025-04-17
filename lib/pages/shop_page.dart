eeadebc97fb8e8e28350987bd0ed794d2dc9a083
Majok-Bol <bolmajok001@gmail.com>
2025-04-17 12:58:16 +0300

    added search icon

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,5 +1,5 @@
 //handle shop page
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget{
   const ShopPage({super.key});
@@ -9,6 +9,44 @@ class ShopPage extends StatefulWidget{
 class ShopPageState extends State<ShopPage>{
   @override
   Widget build(BuildContext context){
-    return Center(child: Text('Shop more for less'),);
+    return Column(children: [
+      //search bar
+      Container(
+        padding: EdgeInsets.all( 20),
+        margin: EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12)
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Search',style: TextStyle(color: Colors.grey),),
+            Icon(Icons.search,color: Colors.grey),
+
+
+          ],
+        ),
+      ),
+      Padding(padding:EdgeInsets.symmetric(vertical: 12),
+      child: Text('Everyone flies..some fly longer than others'),),
+      //hot picks
+      Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25),
+        child:   Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+        Text('Hot picks 🔥',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+        Text('See all',style: TextStyle(color: Colors.blue,fontSize: 24),)
+      ],),),
+      SizedBox(height: 20,),
+      Expanded(child: ListView.builder(
+          itemBuilder: (context,index){
+            return ShoeTile();
+
+      }))
+
+    ],);
   }
 }
