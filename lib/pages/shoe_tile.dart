d32d7a167912e56f88414abbad575dc057e100d3
Majok-Bol <bolmajok001@gmail.com>
2025-04-20 20:41:09 +0300

    changed title

--- lib/pages/shoe_tile.dart
+++ lib/pages/shoe_tile.dart
@@ -1,53 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app/models/shoe.dart';
-class ShoeTile extends StatelessWidget{
+
+class ShoeTile extends StatelessWidget {
   //create shoe instance
   Shoe shoe;
-  ShoeTile({super.key,required this.shoe});
+  ShoeTile({super.key, required this.shoe});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(left: 23),
-      width: 200,
+      padding: EdgeInsets.only(left: 5),
+
       decoration: BoxDecoration(
-          color: Colors.grey.shade100,
-          borderRadius: BorderRadius.circular(12)),
+        color: Colors.grey.shade100,
+        borderRadius: BorderRadius.circular(12),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           //shoe pic
           ClipRRect(
             borderRadius: BorderRadius.circular(12),
-              child: Image.asset(shoe.imagePath)),
+            child: Image.asset(shoe.imagePath),
+          ),
           //description
-          Padding(padding:EdgeInsets.symmetric(horizontal:1),child:Text(shoe.description,style: TextStyle(color: Colors.grey),),),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 1),
+            child: Text(shoe.description, style: TextStyle(color: Colors.grey)),
+          ),
 
-          Padding(padding: EdgeInsets.only(left: 13),child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [    Text(shoe.name,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-                SizedBox(height: 5,),
-                Text('\$'+ shoe.price),],),
-
-          ],),
+          Padding(
+            padding: EdgeInsets.only(left: 1),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      shoe.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 1),
+                    Text('\$' + shoe.price),
+                  ],
+                ),
+              ],
+            ),
           ),
           //price
           //button to add to cart
           Container(
-            padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    bottomRight:Radius.circular(12)),
-                color: Colors.black
+            padding: EdgeInsets.all(1),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                bottomRight: Radius.circular(12),
               ),
+              color: Colors.black,
+            ),
 
-              child: Icon(Icons.add,color: Colors.white,)),
+            child: Icon(Icons.add, color: Colors.white),
+          ),
         ],
-
       ),
     );
   }
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -5,64 +5,84 @@ import 'package:e_commerce_app/pages/shoe_tile.dart';
 import 'package:provider/provider.dart';
 import 'package:e_commerce_app/models/cart.dart';
 
-class ShopPage extends StatefulWidget{
+class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
   @override
-  ShopPageState createState()=>ShopPageState();
+  ShopPageState createState() => ShopPageState();
 }
-class ShopPageState extends State<ShopPage>{
+
+class ShopPageState extends State<ShopPage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     //consume data in the cart
-    return Consumer<Cart>(builder: (context,value,child)=>Column(children: [
-      //search bar
-      Container(
-        padding: EdgeInsets.all( 20),
-        margin: EdgeInsets.symmetric(horizontal: 25),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12)
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Search',style: TextStyle(color: Colors.grey),),
-            Icon(Icons.search,color: Colors.grey),
-
-
-          ],
-        ),
-      ),
-      Padding(padding:EdgeInsets.symmetric(vertical: 12),
-      child: Text('Everyone flies..some fly longer than others'),),
-      //hot picks
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 25),
-        child:   Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-        Text('Hot picks 🔥',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-        Text('See all',style: TextStyle(color: Colors.blue,fontSize: 24),)
-      ],),),
-      SizedBox(height: 20,),
-      Expanded(child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount:4,
-          itemBuilder: (context,index){
-            // Shoe shoe=Shoe(
-            //     name: 'Elegant sneaker',
-            //     price: '200',
-            //     imagePath:'lib/images/p3.jpg',
-            //     description: 'High quality shoe');
-            //get shoe list using its index
-            Shoe shoe=value.getShoeList()[index];
-            return ShoeTile(shoe:shoe);
-
-      })),
-      Padding(padding: EdgeInsets.only(top: 25,left: 25,right: 25),
-      child: Divider(color: Colors.white,),),
-
-    ],),);
+    return Consumer<Cart>(
+      builder:
+          (context, value, child) => Column(
+            children: [
+              //search bar
+              Container(
+                padding: EdgeInsets.all(2),
+                margin: EdgeInsets.symmetric(horizontal: 1),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Search', style: TextStyle(color: Colors.grey)),
+                    Icon(Icons.search, color: Colors.grey),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 2),
+                child: Text('Everyone flies..some fly longer than others'),
+              ),
+              //hot picks
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hot picks 🔥',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'See all',
+                      style: TextStyle(color: Colors.blue, fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 1,
+                  itemBuilder: (context, index) {
+                    // Shoe shoe=Shoe(
+                    //     name: 'Elegant sneaker',
+                    //     price: '200',
+                    //     imagePath:'lib/images/p3.jpg',
+                    //     description: 'High quality shoe');
+                    //get shoe list using its index
+                    Shoe shoe = value.getShoeList()[index];
+                    return ShoeTile(shoe: shoe);
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 1, left: 1, right: 1),
+                child: Divider(color: Colors.white),
+              ),
+            ],
+          ),
+    );
   }
 }
