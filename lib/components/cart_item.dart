bd5b48d9fb25b3dca8f2605e8be4e666eb9556d9
Majok-Bol <bolmajok001@gmail.com>
2025-04-22 13:30:17 +0300

    added delete icon to remove cart item

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,6 +1,8 @@
 //handle cart items
+import 'package:e_commerce_app/models/cart.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app/models/shoe.dart';
+import 'package:provider/provider.dart';
 
 class CartItem extends StatefulWidget {
   Shoe shoe;
@@ -11,12 +13,25 @@ class CartItem extends StatefulWidget {
 }
 
 class CartItemState extends State<CartItem> {
+
+  void removeItemFromCart(){
+    Provider.of<Cart>(context,listen: false).removeItemFromCart(widget.shoe);
+  }
   @override
   Widget build(BuildContext context) {
-    return ListTile(
+    return Container(
+      // color: Colors.greenAccent,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.grey.shade100,
+      ),
+      padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(bottom: 10),
+        child: ListTile(
       leading: Image.asset(widget.shoe.imagePath),
       title: Text(widget.shoe.name),
       subtitle: Text(widget.shoe.price),
-    );
+          trailing: IconButton(onPressed:removeItemFromCart, icon:Icon(Icons.delete)),
+    ));
   }
 }
